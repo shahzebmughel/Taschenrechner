a130772497444302c9bfdb4c6bc9bec9e454d299
Shahzeb <Shahzebmughel@hotmail.com>
2026-04-29 16:44:53 +0200

    Base Calculator functions complete. next features would be: clear and backspace functioniality.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,70 @@ namespace Taschenrechner
 
         private void NumBtn(object sender, RoutedEventArgs e)
         {
+            InputTxt.Text += ((Button)sender).Content;
+        }
+
+        private void NumBtnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            string[] resultArray = InputTxt.Text.Split(new char[] { '+', '-', 'x', '/' }, StringSplitOptions.RemoveEmptyEntries);
+            char operatorV;
+
+            if(InputTxt.Text.Contains("+"))
+            {
+                operatorV = '+';
+            }
+            else if(InputTxt.Text.Contains("-"))
+            {
+                operatorV = '-';
+            }
+            else if(InputTxt.Text.Contains("x"))
+            {
+                operatorV = 'x';
+            }
+            else if(InputTxt.Text.Contains("/"))
+            {
+                operatorV = '/';
+            }
+            else
+            {
+                return;
+            }
+
+            if(resultArray.Length < 2)
+            {
+                return;
+            }
+
+
+            int num1 = int.Parse(resultArray[0]);
+            int num2 = int.Parse(resultArray[1]);
+
+            switch (operatorV)
+            {
+                case '+':
+                    InputTxt.Text = (num1 + num2).ToString();
+                    break;
+                case '-':
+                    InputTxt.Text = (num1 - num2).ToString();
+                    break;
+                case 'x':
+                    InputTxt.Text = (num1 * num2).ToString();
+                    break;
+                case '/':
+                    InputTxt.Text = (num1 / num2).ToString();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void NumBtnClear(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NumBtnBackSpace(object sender, RoutedEventArgs e)
+        {
 
         }
     }
